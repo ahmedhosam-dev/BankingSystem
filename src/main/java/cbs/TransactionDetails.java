348066a6be0a4740f02d6c9e81cdb1cb4e445d1e
ahmedhosam-dev <ahmed0114702@gmail.com>
2024-12-13 22:58:10 +0200

    Create transaction abstract class

---
+++ src/main/java/cbs/TransactionDetails.java
@@ -0,0 +1,50 @@
+package cbs;
+
+import java.sql.Timestamp;
+
+enum TransactionStatus {
+    SUCCESS,
+    FAILED,
+    PENDING
+}
+
+public abstract class TransactionDetails {
+    private final int id;
+    private Account account;
+    private Timestamp transactionDate;
+    private double amount;
+    private TransactionStatus status;
+
+    public TransactionDetails(int id, Account account, Timestamp transactionDate, double amount, TransactionStatus status) {
+        this.id = id;
+        this.account = account;
+        this.transactionDate = transactionDate;
+        this.amount = amount;
+        this.status = status;
+    }
+
+    // Getting members
+    public final int get_id() {
+        return this.id;
+    }
+
+    public final Account get_account() {
+        return this.account;
+    }
+
+    public final Timestamp get_transaction_date() {
+        return this.transactionDate;
+    }
+
+    public final double get_amount() {
+        return this.amount;
+    }
+
+    public final TransactionStatus get_status() {
+        return this.status;
+    }
+
+    // Methods
+    public abstract String display();
+    
+}
