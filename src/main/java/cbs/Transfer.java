952f0a43b2c18dc33e264fb8b8ce325e2d1eeea9
ahmedhosam-dev <ahmed0114702@gmail.com>
2024-12-14 19:55:15 +0200

    Move enum to enum folder, add account id member and getter method for it

--- src/main/java/cbs/Transfer.java
+++ src/main/java/cbs/Transfer.java
@@ -2,16 +2,27 @@ package cbs;
 
 import java.sql.Timestamp;
 
+import cbs.enums.TransactionStatus;
+
 public class Transfer extends TransactionDetails {
     private Account recipient;
+    private int recipient_id;
 
     public Transfer(int id, Account account, Timestamp transactionDate, double amount, TransactionStatus status, Account recipient) {
         super(id, account, transactionDate, amount, status);
         this.recipient = recipient;
     }
+    public Transfer(int id, int account, Timestamp transactionDate, double amount, TransactionStatus status, int recipient) {
+        super(id, account, transactionDate, amount, status);
+        this.recipient_id = recipient;
+    }
 
     // Getting members
     public Account get_recipient() {
         return this.recipient;
+    }
+
+    public int get_recipient_id() {
+        return this.recipient_id;
     }
 }
