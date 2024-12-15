9b7faeababc4ff102cfb12c68adab114dfb93706
ahmedhosam-dev <ahmed0114702@gmail.com>
2024-12-15 21:42:12 +0200

    Make class accept recipient id

--- src/main/java/cbs/Transaction.java
+++ src/main/java/cbs/Transaction.java
@@ -5,15 +5,24 @@ import java.sql.Timestamp;
 import cbs.enums.TransactionStatus;
 import cbs.enums.TransactionType;
 
-public class Transaction extends TransactionDetails{
+public class Transaction extends TransactionDetails {
     private TransactionType type;
+    private int recipient_id;
 
     public Transaction(int id, int account, Timestamp transactionDate, double amount, TransactionStatus status, TransactionType type) {
         super(id, account, transactionDate, amount, status);
         this.type = type;
     }
+    public Transaction(int id, int account, Timestamp transactionDate, double amount, TransactionStatus status, TransactionType type, int recipient_id) {
+        super(id, account, transactionDate, amount, status);
+        this.type = type;
+        this.recipient_id = recipient_id;
+    }
     // Getting members
     public String get_type() {
         return this.type.name();
+    }
+    public int get_recipient_id() {
+        return this.recipient_id;
     }
 }
